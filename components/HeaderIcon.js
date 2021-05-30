b2d632ca459632b0c09579bc2daa857610da6145
“Vesta <vestanassone@gmail.com>
2021-05-30 16:09:43 +0200

    Styles the headerIcon container.

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function HeaderIcon({Icon}) {
     return (
-        <div>
-            <Icon className="h-5"/>
+        <div className="cursor-pointer md:px-10 md:hover:bg-gray-100 sm:h-14 rounded-xl active: border-b-2 active:border-blue-500 group">
+            <Icon className="h-5 text-center sm:h-7 mx-auto group-hover:text-blue-500"/>
         </div>
     )
 }
