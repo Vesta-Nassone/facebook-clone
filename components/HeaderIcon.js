cbdd2e200ab7edcb67b55811809df2a8ece6096a
“Vesta <vestanassone@gmail.com>
2021-05-30 16:30:15 +0200

    Fixes the icon shadow issue.

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderIcon({Icon}) {
     return (
-        <div className="cursor-pointer md:px-10 md:hover:bg-gray-100 sm:h-14 rounded-xl active: border-b-2 active:border-blue-500 group">
+        <div className="cursor-pointer md:px-10 md:hover:bg-gray-100 sm:h-14 rounded-xl active:border-b-2 active:border-blue-500 group">
             <Icon className="h-5 text-center sm:h-7 mx-auto group-hover:text-blue-500"/>
         </div>
     )
