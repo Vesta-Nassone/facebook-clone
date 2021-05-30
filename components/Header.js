a499e226ea7126cb9b9f8f2919f68c753e8b6020
“Vesta <vestanassone@gmail.com>
2021-05-30 15:51:52 +0200

    Styles the SearchIcon.

--- components/Header.js
+++ components/Header.js
@@ -18,12 +18,14 @@ import {
 function Header() {
     return (
         <div>
-            <div>
+            <div className="flex items-center">
                 <Image src="https://links.papareact.com/5me" width={40} 
                 height={40}
                 layout="fixed"/>
-                <div>
+                <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+                    <SearchIcon className="h-6 text-gray-600"/>
                     <input 
+                    className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
                     type="text" 
                     placeholder="Search Facebook"/>
                 </div>
