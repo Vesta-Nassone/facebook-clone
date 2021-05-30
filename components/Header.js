4d9c8a16fff2f894e4cd68dc7eb1ac9380ecc211
“Vesta <vestanassone@gmail.com>
2021-05-30 13:57:40 +0200

    Adds heroicon package and import some icons.

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import Image from "next/image";
+import {
+    BellIcon,
+    ChatIcon,
+    ChevronDownIcon,
+    HomeIcon,
+    UserGroupIcon,
+    ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+    FlagIcon,
+    PlayIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
     return (
         <div>
-            <h1>Header</h1>
             <div>
-                <Image src="https://www.links.papreact.com/5me" width={40} 
+                <Image src="https://links.papareact.com/5me" width={40} 
                 height={40}
                 layout="fixed"/>
+                <div>
+                    <input 
+                    type="text" 
+                    placeholder="Search Facebook"/>
+                </div>
             </div>
         </div>
     );
