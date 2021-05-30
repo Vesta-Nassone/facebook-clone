7b39df1b0863b4dc1be95a282c074a83c5a6e26e
“Vesta <vestanassone@gmail.com>
2021-05-30 16:30:34 +0200

    Adds flex-shrink to input.

--- components/Header.js
+++ components/Header.js
@@ -26,15 +26,17 @@ function Header() {
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text-gray-600"/>
                     <input 
-                    className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+                    className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
                     type="text" 
                     placeholder="Search Facebook"/>
                 </div>
             </div>
             <div className="flex justify-center flex-grow">
-                <div className="flex space-x-6 md:spaxe-x-2">
+                <div className="flex space-x-6 md:space-x-2">
                     <HeaderIcon Icon={HomeIcon}/>
-                    <HeaderIcon Icon={FlagIcon}/><HeaderIcon Icon={PlayIcon}/><HeaderIcon Icon={ShoppingCartIcon}/>
+                    <HeaderIcon Icon={FlagIcon}/>
+                    <HeaderIcon Icon={PlayIcon}/>
+                    <HeaderIcon Icon={ShoppingCartIcon}/>
                     <HeaderIcon Icon={UserGroupIcon}/>
                 </div>
             </div>
