8d143b24fec6d933c16cee9a7ada14c2d2960623
“Vesta <vestanassone@gmail.com>
2021-05-31 23:02:12 +0200

    Adds a Login componet.

---
+++ components/Login.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+function Login() {
+    return (
+        <div>
+            <Image 
+            src="https://links.papareact.com/5me"
+            height={400}
+            width={400}
+            objectFit="contain"/>
+        </div>
+    )
+}
+
+export default Login
