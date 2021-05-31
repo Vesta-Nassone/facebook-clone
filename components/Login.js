7ed9914f916d365d684a76904aa537cef9cfffb9
“Vesta <vestanassone@gmail.com>
2021-05-31 23:29:48 +0200

    Configures the Login.

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,18 @@
 import Image from "next/image";
+import {signIn} from "next-auth/client";
 
 function Login() {
     return (
-        <div>
+        <div className="grid place-items-center">
             <Image 
             src="https://links.papareact.com/5me"
             height={400}
             width={400}
             objectFit="contain"/>
+            <h1 
+            // @ts-ignore
+            onClick={signIn}
+            className="p-5 bg-blue-500 rounded-full text-white text-center cursor-pointer">Login with Facebook</h1>
         </div>
     )
 }
