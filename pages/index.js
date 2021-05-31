cb24e924706ec59976c21c1c9313fbc3821625ff
“Vesta <vestanassone@gmail.com>
2021-05-31 22:26:14 +0200

    Adds server side rendering code to get login session.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from 'next-auth/client';
 import Head from 'next/head'
 import Header from '../components/Header';
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home(session) {
+  if(!session) return <Login/>;
   return (
     <div>
       <Head>
@@ -10,4 +13,17 @@ export default function Home() {
       <Header/>
     </div>
   )
+}
+
+// Server side rendering in NextJS
+export async function getServerSideProps(context){
+  // Get the user
+  const session = await getSession(context);
+
+  return {
+    props:{
+      session
+    }
+  }
+
 }
