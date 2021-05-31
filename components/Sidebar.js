30dfa2bf7e0dce8b866d1f50bc3dc6ae9e3c7cee
“Vesta <vestanassone@gmail.com>
2021-06-01 00:48:52 +0200

    Creates a Sidebar component. Populates the sidebar with icons.

---
+++ components/Sidebar.js
@@ -0,0 +1,39 @@
+import {useSession} from "next-auth/client";
+import {
+    ChevronDoubleDownIcon,
+    ShoppingBagIcon,
+    UserGroupIcon,
+} from "@heroicons/react/outline";
+import {
+    CalendarIcon,
+    ClockIcon,
+    DesktopComputerIcon,
+    UsersIcon,
+    
+} from "@heroicons/react/solid"
+
+function Sidebar() {
+    const [session, loading] = useSession();
+    return (
+        <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+            <SidebarRow 
+            Icon={UsersIcon}
+            title="Friends"/>
+            <SidebarRow 
+            Icon={UserGroupIcon}
+            title="Friends"/><SidebarRow 
+            Icon={ShoppingBagIcon}
+
+            title="Friends"/><SidebarRow 
+            Icon={DesktopComputerIcon}
+            title="Friends"/><SidebarRow 
+            Icon={CalendarIcon}
+            title="Friends"/>
+            <SidebarRow 
+            Icon={ChevronDoubleDownIcon}
+            title="Friends"/>
+        </div>
+    )
+}
+
+export default Sidebar
