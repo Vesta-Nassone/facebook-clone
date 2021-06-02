076fe0a1eff12a41ce800f8b1fcae9c4c8c657a4
“Vesta <vestanassone@gmail.com>
2021-06-02 15:56:05 +0200

    Adds styling to the Feed container.

--- components/Feed.js
+++ components/Feed.js
@@ -2,7 +2,7 @@ import Stories from "./Stories";
 
 function Feed() {
     return (
-        <div>
+        <div className="flex-grow h-screen pb-44 pt-6">
             <div>
                 <Stories/>
             </div>
