0d083bcc3fe3a337eebcea212996e98b0986c0f7
“Vesta <vestanassone@gmail.com>
2021-06-02 15:52:22 +0200

    Pulls in the Stories into Feed.js

--- components/Feed.js
+++ components/Feed.js
@@ -1,8 +1,10 @@
+import Stories from "./Stories";
+
 function Feed() {
     return (
         <div>
             <div>
-                
+                <Stories/>
             </div>
         </div>
     )
