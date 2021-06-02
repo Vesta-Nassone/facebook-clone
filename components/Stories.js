0df25f76c2f63f9df1528a62891cab23313f2257
“Vesta <vestanassone@gmail.com>
2021-06-02 15:38:05 +0200

    Adds the stories Data Structure.

--- components/Stories.js
+++ components/Stories.js
@@ -1,4 +1,31 @@
 function Stories() {
+    const stories = [
+        {
+            name: "Benzo",
+            src: "https://links.papareact.com/zof",
+            profile: "https://links.papareact.com/lv4"
+        },
+        {
+            name: "Elon Musk",
+            src: "https://links.papareact.com/4zn",
+            profile: "https://links.papareact.com/kxk"
+        },
+        {
+            name: "Jeff Bezos",
+            src: "https://links.papareact.com/k2j",
+            profile: "https://links.papareact.com/f0p"
+        },
+        {
+            name: "Mark Zuckerberg",
+            src: "https://links.papareact.com/xql",
+            profile: "https://links.papareact.com/snf"
+        },
+        {
+            name: "Bill Gates",
+            src: "https://links.papareact.com/4u4",
+            profile: "https://links.papareact.com/zyv"
+        }
+    ]
     return (
         <div>
             
