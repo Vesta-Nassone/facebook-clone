06368aee9eb5273d468a5fd8625ce507826346e3
“Vesta <vestanassone@gmail.com>
2021-06-02 15:51:57 +0200

    Fixes the lyout="fill" issue.

--- components/StoryCard.js
+++ components/StoryCard.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function StoryCard({name, src, profile}) {
     return (
-        <div className="relative">
+        <div className="relative h-14 w-14">
             <Image
             className="absolute opacity-0 lg:opacity-100 rounded-full z-50 top-10"
             src={profile}
@@ -14,10 +14,7 @@ function StoryCard({name, src, profile}) {
             <Image
             className="object-cover filter brightness-75 rounded-full lg:rounded-3xl"
             src={src}
-            width={40}
-            height={40}
             layout="fill"
-            objectFit="cover"
             />
         </div>
     )
